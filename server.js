630b57c0116e905b1a657920d3037c7ef77073dd
Vladimir Litvinchik <litvinchik.vladimir@gmail.com>
2014-02-15 14:59:39 +0200

    MAEN skeleton

--- server.js
+++ server.js
@@ -1,19 +1,52 @@
 /**
  * Created by vlitvinchik on 2/12/14.
  */
-var express  = require('express');
+var express  = require('express'),
+    stylus = require('stylus'),
+    mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+function compile(str, path){
+    return stylus(str).set('filename', path);
+}
+
 app.configure(function(){
     app.set('views', __dirname + '/server/views');
     app.set('view engine', 'jade');
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(stylus.middleware({
+         src: __dirname + '/public',
+         compile: compile
+    }));
+    app.use(express.static(__dirname + '/public'));
+});
+
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, "Connection error..."));
+db.once('open', function callback(){
+    console.log('MultiVision db opened...')
+});
+
+var messageSchema = mongoose.Schema({message: String});
+var Model = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Model.findOne().exec(function(err, messageDoc){
+    mongoMessage = messageDoc;
+});
+
+app.get('/partials/:partialPath', function(req, res){
+    res.render('partials/' + req.params.partialPath);
 });
 
 app.get('*', function(req, res){
-   res.render('index');
+   res.render('index', {
+       mongoMessage: mongoMessage
+   });
 });
 
 var port = 3030;
